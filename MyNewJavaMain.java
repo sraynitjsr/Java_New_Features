673ea6cd3dd380d3006b5b5778c6881fd21a2ef9
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-05-02 23:42:28 +0530

    Implementing MyNewJavaMain.java

--- MyNewJavaMain.java
+++ MyNewJavaMain.java
@@ -1,5 +1,6 @@
 public class MyNewJavaMain{
      public static void main(String []args){
         System.out.println("Hello World, This is Java 20, the New Java");
+        MyVirtualThread.Start();
      }
 }
