c70f556947fad8fdc16e24778ff83a9d987ff4b7
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-28 07:58:52 +0530

    Java Virtual Threads

--- MyNewJavaMain.java
+++ MyNewJavaMain.java
@@ -1,5 +1,5 @@
 public class MyNewJavaMain{
-     public static void main(String []args){
+     public static void main(String []args) throws InterruptedException{
         System.out.println("Hello World, This is Java 20, the New Java");
         MyVirtualThread.Start();
         MyRecordPatterns.start();
