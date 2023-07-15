417cec63722c8900e9a3a35cd6a0ae36fe19108e
unknown <subhradeepray2017@gmail.com>
2023-07-15 05:52:27 +0530

    Java Hello World, Without Class and PSVM

---
+++ MyMain.java
@@ -0,0 +1,8 @@
+/*
+    javac --release 21 --enable-preview MyMain.java
+    java --enable-preview MyMain
+*/
+
+void main() {
+    System.out.println("Hello World, This is Java 21");
+}
--- MyNewJavaMain.java
+++
@@ -1,7 +0,0 @@
-public class MyNewJavaMain{
-     public static void main(String []args) throws InterruptedException{
-        System.out.println("Hello World, This is Java 20, the New Java");
-        MyVirtualThread.Start();
-        MyRecordPatterns.start();
-     }
-}
